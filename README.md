3b1735498847f27180b5670c7b8a05f057f1665d
Simon McKenna <simes@hotmail.co.uk>
2016-12-05 20:46:44 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# py-marantz-avr-control
+Python Code for driving IP Interface for Marantz AVR (MCR510)
+
+The code was derived from a project https://github.com/iamcanadian2222/MarantzIP
+
+but that didn't seem to work for me. 
+
+The Marantz control codes are published here:
+
+http://www.awe-europe.com/documents/Control%20Docs/Marantz/Marantz_AV_SR_NR_PROTOCOL_V02.xls
+
+Simple ones seem to work, though cannot change source at the moment
+
+todo - tidy up and fix - the eventaul goal is to integrate with flask-ask and alexa.
+
+ 
---
+++ pyamp.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+import telnetlib, time,threading,re
+import sys
+
+device={}
+
+class ampControl():
+
+    def __init__(self,ip,port):
+        self.ip = ip
+        self.port = port
+        self.timer = None
+        self.conn =  telnetlib.Telnet()
+
+    def connect(self):
+        self.start_timer()
+        self.conn.open(self.ip, self.port, 3)
+        print("[CONNECTED]")
+        return self.conn
+
+    def disconnect(self):
+        self.conn.close()
+        print("Disconnected")
+        self.timer.cancel()
+        self.timer = None
+
+    def start_timer(self):
+        self.timer = threading.Timer(10, self.disconnect)
+        self.timer.start()
+
+    def reset_timer(self):
+        print("Resetting timer.")
+        self.timer.cancel()
+        self.start_timer()
+
+    def send_command(self,command):
+        t = self.connect()
+        t.write(command.encode())
+        self.disconnect()
+       
+    def set_mute(self,onoff):
+        if onoff.upper() not in ["ON", "OFF"]:
+           print("bad mute:",onoff)
+           return "INVALID"
+        self.send_command("MU" + onoff.upper() +"\r")
+        return "OK"
+
+    def set_power(self,onstandby):
+        if onstandby.upper() not in ["ON", "STANDBY"]:
+           print("bad power:",onstandby)
+           return "INVALID"
+        self.send_command("PW" + onstandby.upper() +"\r")
+        return "OK"
+
+    def set_input(self,input):
+        if input.upper() not in [ "USB", "TUNER","V.AUX","MPLAY"]:
+           print("bad input:",input)
+           return "INVALID"
+        self.send_command("SI" + input.upper() +"\r")
+        return "OK"
+
+    def set_volume(self,level):
+        if level.upper() not in [ "UP", "DOWN"]:
+          if re.search(r'[0-9][0-9]',level) == None:
+             print("bad input:",input)
+             return "INVALID"
+        self.send_command("MV" + level.upper() +"\r")
+        return "OK"
+
+
+#main()
+
+amp = ampControl("192.168.1.106",23)
+
+#amp.set_mute("on")
+
+#time.sleep(5)
+
+#amp.set_mute("off")
+
+#time.sleep(5)
+
+#amp.set_volume("up")
+
+#time.sleep(5)
+
+#amp.set_volume("down")
+
+#time.sleep(5)
+
+#amp.set_input("MPLAY")
+
+#time.sleep(5)
+
+#amp.set_input("V.AUX")
+
+#time.sleep(5)
+
+#amp.set_volume("10")
+
+#time.sleep(5)
+
+#amp.set_volume("50")
+
+#time.sleep(5)
+
+amp.set_power("standby")
